7f8dc4705da2223723ce9f35926865e2d048e9a5
lamnguyen231 <mysteryme2312@gmail.com>
2026-05-08 17:27:00 +0700

    Add store product validation and controller
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ ImplementationPhase/TheALMAProject/TheALMAProject.Application/Validator/StoreProductValidators/CreateStoreProductDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheALMAProject.Application.DTOs.StoreProductDtos;
+
+namespace TheALMAProject.Application.Validator.StoreProductValidators
+{
+    public class CreateStoreProductDtoValidator : AbstractValidator<CreateStoreProductDto>
+    {
+        public CreateStoreProductDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Tên sản phẩm không được để trống")
+                .MaximumLength(100).WithMessage("Tên sản phẩm tối đa 100 ký tự");
+
+            RuleFor(x => x.Price)
+                .NotEmpty().WithMessage("Giá cả không được để trống")
+                .GreaterThan(0).WithMessage("Sản phẩm phải có giá trị lớn hơn 0");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Miêu tả sàn phẩm tối đa 500 kí tự");
+
+            RuleFor(x => x.BaseProductId)
+                .NotNull().WithMessage("Phải có sản phẩm nền")
+                .GreaterThan(0).WithMessage("Lựa chọn ID phải lớn hơn 0");
+
+            RuleFor(x => x.UniversityId)
+                .NotNull().WithMessage("Phải có trường đại học")
+                .GreaterThan(0).WithMessage("Lựa chọn ID phải lớn hơn 0");
+        }
+    }
+}
