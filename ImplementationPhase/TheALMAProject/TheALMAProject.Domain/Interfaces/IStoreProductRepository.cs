057305ed9ac03c8d83fb16cacaa7bd46e659412a
lamnguyen231 <mysteryme2312@gmail.com>
2026-05-08 17:27:00 +0700

    Add store product repository support
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ ImplementationPhase/TheALMAProject/TheALMAProject.Domain/Interfaces/IStoreProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using TheALMAProject.Domain.Common;
+using TheALMAProject.Domain.Queries;
+using TheALMAProject.Infrastructure.Models;
+
+namespace TheALMAProject.Domain.Interfaces
+{
+    public interface IStoreProductRepository
+    {
+        Task<PagedResult<StoreProduct>> GetStoreProducts(StoreProductQuery query);
+
+        Task<StoreProduct?> GetById(int id);
+
+        Task<StoreProduct?> GetStoreProductByName(string name);
+
+        Task CreateStoreProduct(StoreProduct storeProduct);
+
+        void UpdateStoreProduct(StoreProduct storeProduct);
+
+        void DeleteStoreProduct(StoreProduct storeProduct);
+    }
+}
